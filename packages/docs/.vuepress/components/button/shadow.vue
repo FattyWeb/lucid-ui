6df546a74dfb5bb624af798f142b53498f84d6be
w12w.com <904>
2023-03-03 16:33:27 +0800

    替换文案

--- packages/docs/.vuepress/components/button/shadow.vue
+++ packages/docs/.vuepress/components/button/shadow.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="center">
-    <vs-button shadow :active="active == 0" @click="active = 0" >Active</vs-button>
-    <vs-button shadow :active="active == 1" @click="active = 1" >Default</vs-button>
-    <vs-button shadow disabled >Disabled</vs-button>
+    <vs-button shadow :active="active == 0" @click="active = 0" >激活</vs-button>
+    <vs-button shadow :active="active == 1" @click="active = 1" >默认</vs-button>
+    <vs-button shadow disabled >禁用</vs-button>
   </div>
 </template>
 <script>
